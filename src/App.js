990ebcd4bcee5301cf9c0fa5e8abf69609ce3f15
huangnuoen <huangnuoen@daishujiankang.com>
2018-11-02 10:00:54 +0800

    transitiongroup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,55 @@
 import React, { Component, Fragment } from "react";
-import { CSSTransition } from "react-transition-group";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import "./style.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      list: [],
       show: true
     };
     this.handleToggle = this.handleToggle.bind(this);
+    this.handleAddItem = this.handleAddItem.bind(this);
   }
   render() {
     return (
       <Fragment>
-        <CSSTransition
-          in={this.state.show}
-          timeout={1000}
-          classNames="fade"
-          unmountOnExit
-          onEntered={el=>{el.style.color='blue'}}
-          appear={true}
-        >
-          <div> hello world </div>
-        </CSSTransition>
-        <button onClick={this.handleToggle}>toggle</button>
+        <TransitionGroup>
+          {this.state.list.map((item, i) => {
+            return (
+              <CSSTransition
+                timeout={5000}
+                classNames="fade"
+                unmountOnExit
+                onEntered={el => {
+                  el.style.color = "blue";
+                }}
+                appear={true}
+                key={i}
+              >
+                <div>
+                  {item}
+                  {i}
+                </div>
+              </CSSTransition>
+            );
+          })}
+        </TransitionGroup>
+        <button onClick={this.handleAddItem}>toggle</button>
       </Fragment>
     );
   }
   handleToggle() {
     this.setState({
       show: !this.state.show
+    });
+  }
+  handleAddItem() {
+    this.setState(prevState => {
+      return {
+        list: [...prevState.list, "item"]
+      };
     });
   }
 }
