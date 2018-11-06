6395bc7ae7df0ede1631fe6270a76907d554defe
huangnuoen <1261290560@qq.com>
2018-11-06 22:44:29 +0800

    reducer action

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { createStore } from "redux";
 import reducer from "./reducer";
+// store必须是唯一的
+// 只有store能改变自己，reducer不能改变store内容
+// reducer必须是纯函数（给固定的输入，就一定会有固定的输入，而且不能有任何副作用）
 const store = createStore(
   reducer /* preloadedState, */,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
--- src/todolist.js
+++ src/todolist.js
@@ -3,6 +3,11 @@ import React, { Component, Fragment } from "react";
 import "antd/dist/antd.css";
 import { Input, Button, List } from "antd";
 import store from "./store";
+import {
+  getInputChangeAction,
+  getAddItemAction,
+  getDeleteItemAction
+} from "./store/actionCreators";
 // import TodoItem from "./todoitem";
 // import "./style.css";
 // import axios from "axios";
@@ -11,6 +16,8 @@ class Todolist extends Component {
   constructor(props) {
     super(props);
     this.state = store.getState();
+    // 订阅store变化信息
+    store.subscribe(this.handleStoreChange.bind(this));
   }
 
   render() {
@@ -21,26 +28,49 @@ class Todolist extends Component {
             value={this.state.inputValue}
             placeholder="todo sth"
             style={{ width: "300px", marginRight: "10px" }}
-            onChange={(e) => this.handleInputChange(e)}
+            onChange={e => this.handleInputChange(e)}
           />
-          <Button type="primary">提交</Button>
+          <Button
+            type="primary"
+            onClick={() => {
+              this.handleBtnClick();
+            }}
+          >
+            提交
+          </Button>
         </div>
         <List
           bordered
           dataSource={this.state.list}
-          renderItem={item => <List.Item>{item}</List.Item>}
+          renderItem={(item, index) => (
+            <List.Item
+              onClick={() => {
+                this.handleItem(index);
+              }}
+            >
+              {item}
+            </List.Item>
+          )}
         />
       </div>
     );
   }
+  // 处理store变化
+  handleStoreChange() {
+    this.setState(store.getState());
+  }
+  handleItem(index) {
+    const action = getDeleteItemAction(index)
+    store.dispatch(action);
+  }
   handleInputChange(e) {
     // 创建action
-    const action = {
-      type: 'change_input_value',
-      value: e.target.value
-    }
-    store.dispatch(action)
-    console.log(e.target.value);
+    const action = getInputChangeAction(e.target.value);
+    store.dispatch(action);
+  }
+  handleBtnClick() {
+    const action = getAddItemAction()
+    store.dispatch(action);
   }
 }
 export default Todolist;
