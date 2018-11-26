7c306858cee289e5b0abfcc3aa02c9b976dbc4c9
huangnuoen <1261290560@qq.com>
2018-11-26 14:04:57 +0800

    py aiohttp

--- py/awesome-python3-webapp/www/app.py
+++ py/awesome-python3-webapp/www/app.py
@@ -21,12 +21,14 @@ from aiohttp import web
 def index(request):
     return web.Response(body='<h1>AWesome</h1>'.encode('utf-8'), content_type='text/html')
 
+
 # 把一个generator标记为coroutine类型，然后，我们就把这个coroutine扔到EventLoop中执行
+# aiohttp的初始化函数init()是个coroutine
 @asyncio.coroutine
 def init(loop):
     app = web.Application()
     app.router.add_route('GET', '/', index)
-    # 开启服务
+    # loop.create_server()利用asyncio创建TCP服务。
     srv = yield from loop.create_server(app._make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
